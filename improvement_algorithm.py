4793e5f2f5e274682264379cc63f79fcda2dc03f
Barbaros Şair <103073388+sairbarbaros@users.noreply.github.com>
2022-04-23 14:40:52 +0300

    Create improvement_algorithm.py
    This code improves the original Tremaux algorithm by noting nodes having three or four open paths. Then, it prevents the robot from going around dead-end traps multiple times. If the node is a beginning of a trap, the code removes this node from the nodes list. Therefore, only safe nodes stay on the list.

---
+++ improvement_algorithm.py
@@ -0,0 +1,82 @@
+i = 0
+nodes = []
+node = 1
+# in competition, robot will take the inputs from virtual map and motor.
+"""
+if there are 3 or 4 open paths:
+    nodes.append(a, b)
+    node = 1
+
+else:
+    node = 0
+"""
+inp = int(input("1-2-3-4"))
+moves = []
+def fwd(a):
+    a += 1
+    return "fwd"
+
+def back(a):
+    a -= 1
+    return "back"
+
+def right(a):
+    a += 1
+    return "right"
+
+def left(a):
+    a -= 1
+    return "left"
+
+
+
+if node == 0:
+    """
+    
+    
+    """
+
+elif node == 1:
+
+    if inp == 1:
+        fwd(i)
+        moves.append(fwd(i))
+
+    elif inp == 2:
+        right(i)
+        moves.append(right(i))
+
+    elif inp == 3:
+        back(i)
+        moves.append(back(i))
+
+    elif inp == 3:
+        left(i)
+        moves.append(left(i))
+
+    if (i == 0) and ("fwd" in moves and "right in moves" and "left" in moves and "back" in moves):
+        hr = moves[0]
+
+        if hr == "fwd":
+            back(i)
+
+        elif hr == "back":
+            fwd(i)
+
+        elif hr == "right":
+            left(i)
+
+        elif hr == "left":
+            right(i)
+
+        nodes.remove(node)
+
+
+
+
+
+
+
+
+
+
